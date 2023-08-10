72e46de4cf3b4c74244bac2730cf3c541440081e
YansTods <rachrisgamer@gmail.com>
2023-08-10 12:40:33 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+var fileInput = document.getElementById("fileInput");
+var playBtn = document.getElementById("playBtn");
+var pauseBtn = document.getElementById("pauseBtn");
+var stopBtn = document.getElementById("stopBtn");
+
+var mediaElement = document.createElement(fileInput.files[0].type.startsWith("audio") ? "audio" : "video");
+mediaElement.controls = true;
+
+fileInput.addEventListener("change", function() {
+    var file = fileInput.files[0];
+    
+    var objectURL = URL.createObjectURL(file);
+    mediaElement.src = objectURL;
+    
+    playBtn.disabled = false;
+    pauseBtn.disabled = false;
+    stopBtn.disabled = false;
+});
+
+playBtn.addEventListener("click", function() {
+  mediaElement.play();
+});
+
+pauseBtn.addEventListener("click", function() {
+  mediaElement.pause();
+});
+
+stopBtn.addEventListener("click", function() {
+  mediaElement.pause();
+  mediaElement.currentTime = 0;
+});
+
+document.querySelector(".player").appendChild(mediaElement);
