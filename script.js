7d32e2347416b634bd96a39f76911a09009d725a
YansTods <rachrisgamer@gmail.com>
2023-08-10 13:15:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
+// Kode JavaScript yang telah diberikan sebelumnya
+
 var fileInput = document.getElementById("fileInput");
 var playBtn = document.getElementById("playBtn");
 var pauseBtn = document.getElementById("pauseBtn");
 var stopBtn = document.getElementById("stopBtn");
-
+var loadingElement = document.getElementById("loading");
 var mediaElement = document.createElement(fileInput.files[0].type.startsWith("audio") ? "audio" : "video");
 mediaElement.controls = true;
+document.querySelector(".player").appendChild(mediaElement);
 
-fileInput.addEventListener("change", function() {
-    var file = fileInput.files[0];
-    
-    var objectURL = URL.createObjectURL(file);
-    mediaElement.src = objectURL;
+fileInput.addEventListener("change", function(e) {
+  // Kode event listener change
+});
 
-    mediaElement.addEventListener("loadedmetadata", function() {
-        playBtn.disabled = false;
-        pauseBtn.disabled = false;
-        stopBtn.disabled = false;
-    });
+playBtn.addEventListener("click", function() {
+  // Kode event listener click playBtn
+});
+
+pauseBtn.addEventListener("click", function() {
+  // Kode event listener click pauseBtn
+});
+
+stopBtn.addEventListener("click", function() {
+  // Kode event listener click stopBtn
+});
+
+fileInput.addEventListener("change", function(e) {
+  var file = fileInput.files[0];
+
+  if (file) {
+    var reader = new FileReader();
+
+    reader.onloadstart = function() {
+      loadingElement.style.display = "block";
+    }
+
+    reader.onload = function(e) {
+      mediaElement.src = e.target.result;
+      loadingElement.style.display = "none";
+      playBtn.disabled = false;
+      pauseBtn.disabled = false;
+      stopBtn.disabled = false;
+    }
+
+    reader.readAsDataURL(file);
+  }
 });
 
 playBtn.addEventListener("click", function() {
@@ -31,5 +59,3 @@ stopBtn.addEventListener("click", function() {
   mediaElement.pause();
   mediaElement.currentTime = 0;
 });
-
-document.querySelector(".player").appendChild(mediaElement);
